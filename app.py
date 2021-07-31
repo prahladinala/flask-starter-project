44626d51d4bc6c979b7cea0ea049fd225237164e
prahladinala <prahlad2018@gmail.com>
2021-07-31 14:23:05 +0530

    Flask Routes

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
+# from flask import Flask
+# app=Flask(__name__)
+
+# @app.route('/')
+# def welcome():
+#     return "Welcome to Flask"
+
+# if __name__ == "__main__":
+#     app.run()
+
 from flask import Flask
-app=Flask(__name__)
+# Creates WSGI Application
+app = Flask(__name__)
+
+# @app.route(rule, options)
+
+# decorator with binding with function
+
 
 @app.route('/')
 def welcome():
     return "Welcome to Flask"
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/new')
+def prahlad():
+    return "Welcome to Prahlad Inala"
+
+
+# App Starts here
+if __name__ == '__main__':
+    # app.run()
+    app.run(debug=True)
